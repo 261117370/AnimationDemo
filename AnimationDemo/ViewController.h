a548d5f053713896a6c54957c6003843c769b595
foxwang <apple@applematoiMac.local>
2012-03-02 15:55:49 +0800

    Initial Commit

---
+++ AnimationDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AnimationDemo
+//
+//  Created by apple on 12-3-2.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
